5b9ab80ac0253407ed075d91c93e3e6a942d6e73
Romain Beaubois <refbeaubois@yahoo.com>
2021-07-12 16:54:28 +0200

    Clean master branch

--- _build/html/_sources/README.md.txt
+++
@@ -1,2 +0,0 @@
-# Guidelines, good practices and tips
-Here is the link : [Guidelines pages](https://ceramic-blue-tim.github.io/Guidelines/)
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-
 import os
 import sys
 import sphinx_rtd_theme
@@ -40,14 +39,13 @@ extensions = ['sphinxcontrib.matlab',
 'sphinx.ext.autosummary',
 'sphinx.ext.extlinks']
 
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -61,6 +59,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
 html_logo = '_static/blue.png'
 html_static_path = ['_static']
 source_suffix = {
